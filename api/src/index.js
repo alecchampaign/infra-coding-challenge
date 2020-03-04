ebd011df318985bff5eb80889bab9a99b094b8ca
Alec Champaign <alecchampaign@protonmail.com>
2020-03-04 17:58:10 -0500

    Refactor server to run as a node cluster

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,27 @@
 const server = require('./server');
+const cluster = require('cluster');
+const coreCount = require('os').cpus().length;
 const mongo = require('./clients/mongo');
 
 (async () => {
-    await mongo.connect();
-    await mongo.flushCollections();
-    
+  await mongo.connect();
+  await mongo.flushCollections();
+
+  if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+
+    for (let i = 0; i < coreCount; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', worker =>
+      console.log(`Worker ${worker.process.pid} has died`)
+    );
+  } else {
     server.listen(9000, () => {
-        console.log('server listening on port 9000');
+      console.log('server listening on port 9000');
     });
+
+    console.log(`Worker ${process.pid} started`);
+  }
 })();
